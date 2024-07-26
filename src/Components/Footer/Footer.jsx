db1e378f9478d998ebd483f5c2befa599e41d3ad
Meet Khetani <147489256+Meet5703@users.noreply.github.com>
2024-07-26 12:53:06 +0000

    234

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,96 +1,70 @@
-import {
-  Button,
-  Container,
-  Heading,
-  HStack,
-  Icon,
-  IconButton,
-  Stack,
-  Text,
-  VStack,
-} from '@chakra-ui/react';
+import { Container, Heading, Icon, List, Text, Stack } from '@chakra-ui/react';
 import React from 'react';
+import { CgInstagram, CgFacebook } from 'react-icons/cg';
 import { Link } from 'react-router-dom';
-import { IoLogoFacebook } from 'react-icons/io';
-import { RiInstagramFill, RiTwitterFill, RiTwitterLine } from 'react-icons/ri';
+
 const Footer = () => {
   return (
-    <Container maxW={'container.xl'} paddingX={0} mt={10}>
+    <Container
+      maxW={'100vw'}
+      backgroundColor={'blue.100'}
+      pt={20}
+      pb={4}
+      px={[3, 3, 3, 28]}
+    >
       <Stack
-        direction={['column', 'column', 'column', 'row']}
-        w={'full'}
+        flexDir={['column', 'column', 'column', 'row']}
         justifyContent={'space-between'}
-        alignItems={'center'}
+        alignItems={'start'}
+        gap={10}
       >
+        <Heading
+          children="SUGAM"
+          fontFamily={'Asap'}
+          fontWeight={'500'}
+          letterSpacing={'2px'}
+          color={'blue.500'}
+        />
         <Stack
+          spacing={[5, 5, 5, 20]}
+          fontSize={20}
+          textTransform={'uppercase'}
           direction={['column', 'column', 'column', 'row']}
-          justifyContent={'space-between'}
-          w={'full'}
         >
-          <Heading
-            children="SUGAM"
-            fontFamily={'Asap'}
-            fontWeight={'500'}
-            letterSpacing={'2px'}
-            color={'blue.400'}
-          />
-          <HStack maxW={'100px'} spacing={6}>
-            <Link to={'/'}>
-              <Text color={'blue.400'} colorScheme="blue" variant={'ghost'}>
-                Home
-              </Text>
-            </Link>
-            <Link to={'/products'}>
-              {' '}
-              <Text color={'blue.400'} colorScheme="blue" variant={'ghost'}>
-                Products
-              </Text>
-            </Link>
-            <Link to={'/about'}>
-              {' '}
-              <Text color={'blue.400'} colorScheme="blue" variant={'ghost'}>
-                About
-              </Text>
-            </Link>
-            <Link to={'/contact'}>
-              {' '}
-              <Text color={'blue.400'} colorScheme="blue" variant={'ghost'}>
-                Contact
-              </Text>
-            </Link>
-          </HStack>
-          <HStack spacing={4}>
-            <Link to={'https://www.facebook.com/sugam.io/'}>
-              <Icon
-                _hover={{
-                  color: 'blue.500',
-                }}
-                boxSize={'30px'}
-                as={IoLogoFacebook}
-              />
-            </Link>
-            <Link to={'https://twitter.com/sugam_io'}>
-              <Icon
-                as={RiInstagramFill}
-                boxSize={'30px'}
-                _hover={{ color: 'blue.500' }}
-              />
-            </Link>
-            <Link to={'https://twitter.com/sugam_io'}>
-              <Icon
-                as={RiTwitterFill}
-                boxSize={'30px'}
-                _hover={{ color: 'blue.500' }}
-              />
-            </Link>
-          </HStack>
+          <Link to={'/'}>
+            <Text>Home</Text>
+          </Link>
+          <Link to={'/products'}>
+            <Text>Products</Text>
+          </Link>
+          <Link to={'/about'}>
+            <Text>About</Text>
+          </Link>
+          <Link to={'/contact'}>
+            <Text>Contact</Text>
+          </Link>
         </Stack>
-        <Text opacity={'0.8'}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam,
-          soluta at quod tempore esse enim.
-        </Text>
-        <Text opacity={'0.8'}>© 2021 Sugam. All rights reserved. </Text>
+        <Stack direction={'row'} gap={6} fontSize={28}>
+          <Link to={'instagram.com'}>
+            <Text borderRadius={'full'} p={2} backgroundColor={'blue.200'}>
+              <CgInstagram />
+            </Text>
+          </Link>
+          <Link to={'facebook.com'}>
+            <Text borderRadius={'full'} p={2} backgroundColor={'blue.200'}>
+              <CgFacebook />
+            </Text>
+          </Link>
+        </Stack>
       </Stack>
+      <Text
+        mt={10}
+        fontWeight={'600'}
+        textAlign={'center'}
+        children={'© 2022 SUGAM. All rights reserved'}
+        color={'blue.500'}
+        fontSize={14}
+      />
     </Container>
   );
 };
